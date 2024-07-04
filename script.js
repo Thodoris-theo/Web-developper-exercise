6847d3a09d1d64a089c14e196eb80c1a84c23cdd
Thodoris Theodorellis <121406512+Thodoris-theo@users.noreply.github.com>
2024-07-04 15:29:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function handleSubmit(event) {
 
     if(inputValue<=10)
         alert("This number is less or equal to 10");
-        else
-            confirm('The number is valid');
+            else
+                confirm('The number is valid');
     }
     window.onload = () => {
         document.getElementById('form-box').addEventListener('submit', handleSubmit);
