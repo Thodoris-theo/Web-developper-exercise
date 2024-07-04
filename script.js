6f416511bea1ad7ee5c5a512aafb80a0764d25b6
Thodoris Theodorellis <121406512+Thodoris-theo@users.noreply.github.com>
2024-07-04 15:14:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function handleSubmit(event) {
+    event.preventDefault(); 
+    const inputValue = document.getElementById('form-input').value;
+
+    if(inputValue<=10)
+        alert("This number is less or equal to 10");
+        else
+            confirm('The number is valid');
+    }
+    window.onload = () => {
+        document.getElementById('form-box').addEventListener('submit', handleSubmit);
+    };
